82513f32b9e6aa5a4c8b1ee13b9bc383bc5a5f8b
shuangji <shuangji@pinduoduo.com>
2023-03-22 16:22:22 +0800

    update

---
+++ learn-netty/src/main/java/com/shengsiyuan/netty/sixthexample/TestServerInitializer.java
@@ -0,0 +1,27 @@
+package com.shengsiyuan.netty.sixthexample;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.protobuf.ProtobufDecoder;
+import io.netty.handler.codec.protobuf.ProtobufEncoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
+import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
+
+/**
+ * @author sj
+ */
+public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline pipeline = socketChannel.pipeline();
+
+        pipeline.addLast(new ProtobufVarint32FrameDecoder());
+        pipeline.addLast(new ProtobufDecoder(MyDataInfo.MyMessage.getDefaultInstance()));
+        pipeline.addLast(new ProtobufVarint32LengthFieldPrepender());
+        pipeline.addLast(new ProtobufEncoder());
+
+        pipeline.addLast(new TestServerHandler());
+    }
+}
